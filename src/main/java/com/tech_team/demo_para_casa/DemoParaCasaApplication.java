85326779b6d55fb3be672a5a29d7f46df6d88d53
JacobUoc <jbajog@uoc.edu>
2022-10-19 22:29:31 +0200

    Primer commit Jacobo

---
+++ src/main/java/com/tech_team/demo_para_casa/DemoParaCasaApplication.java
@@ -0,0 +1,13 @@
+package com.tech_team.demo_para_casa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParaCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoParaCasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech_team/demo_para_casa/DemoParaCasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_team.demo_para_casa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParaCasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
